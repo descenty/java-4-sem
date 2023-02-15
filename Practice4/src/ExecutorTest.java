80a8d80baca9ee7bfa1c674482d4c6402d24acfb
Deverty <bychenkov.a.k@edu.mirea.ru>
2023-02-15 15:41:14 +0300

    practice 4

--- Practice4/src/ExecutorTest.java
+++ Practice4/src/ExecutorTest.java
@@ -1,17 +1,17 @@
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class ExecutorTest {
-    public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
-        executorService.submit(() -> {
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("We run it");
-        });
-        executorService.submit(() -> System.out.println("Start"));
+    public static void main(String[] args) throws InterruptedException {
+        MyExecutorService executorService = new MyExecutorService(5);
+        for (int i = 0; i < 10; i++) {
+            executorService.submit(() -> {
+                System.out.println(Thread.currentThread().getName() + ": Task started");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + ": Task completed");
+            });
+        }
     }
 }
--- Practice4/src/MyExecutorService.java
+++ Practice4/src/MyExecutorService.java
@@ -1,19 +1,32 @@
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
-public class MyExecutorService implements ExecutorService {
+public class MyExecutorService {
+    class Worker extends Thread {
+        public Worker(String name) {
+            super(name);
+        }
+
+        @Override
+        public void run() {
+            while (!isShutdown) {
+                try {
+                    Runnable task = taskQueue.take();
+                    task.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     private final BlockingQueue<Runnable> taskQueue;
-    private final List<Thread> workerThreads;
+    private final List<Worker> workerThreads;
     private boolean isShutdown = false;
 
     public MyExecutorService(int nThreads) {
@@ -21,25 +34,12 @@ public class MyExecutorService implements ExecutorService {
         workerThreads = new ArrayList<>();
 
         for (int i = 0; i < nThreads; i++) {
-            Thread workerThread = new Thread();
+            Worker workerThread = new Worker("Worker-" + (i + 1));
             workerThreads.add(workerThread);
             workerThread.start();
         }
     }
 
-    @Override
-    public void execute(Runnable command) {
-        if (isShutdown) {
-            throw new IllegalStateException("ExecutorService is shutdown");
-        }
-        try {
-            taskQueue.put(command);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
     public void shutdown() {
         isShutdown = true;
         for (Thread workerThread : workerThreads) {
@@ -47,39 +47,14 @@ public class MyExecutorService implements ExecutorService {
         }
     }
 
-    @Override
-    public List<Runnable> shutdownNow() {
-        isShutdown = true;
-        for (Thread workerThread : workerThreads) {
-            workerThread.interrupt();
-        }
-        return new ArrayList<>(taskQueue);
-    }
-
-    @Override
     public boolean isShutdown() {
         return isShutdown;
     }
 
-    @Override
     public boolean isTerminated() {
         return isShutdown && taskQueue.isEmpty();
     }
 
-    @Override
-    public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
-        long timeoutInMs = unit.toMillis(timeout);
-        long startTime = System.currentTimeMillis();
-        while (System.currentTimeMillis() - startTime < timeoutInMs) {
-            if (isTerminated()) {
-                return true;
-            }
-            Thread.sleep(100);
-        }
-        return false;
-    }
-
-    @Override
     public <T> Future<T> submit(Callable<T> task) {
         if (isShutdown) {
             throw new IllegalStateException("ExecutorService is shutdown");
@@ -93,21 +68,6 @@ public class MyExecutorService implements ExecutorService {
         return futureTask;
     }
 
-    @Override
-    public <T> Future<T> submit(Runnable task, T result) {
-        if (isShutdown) {
-            throw new IllegalStateException("ExecutorService is shutdown");
-        }
-        FutureTask<T> futureTask = new FutureTask<>(task, result);
-        try {
-            taskQueue.put(futureTask);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return futureTask;
-    }
-
-    @Override
     public Future<?> submit(Runnable task) {
         if (isShutdown) {
             throw new IllegalStateException("ExecutorService is shutdown");
@@ -119,98 +79,5 @@ public class MyExecutorService implements ExecutorService {
             e.printStackTrace();
         }
         return futureTask;
-    }
-
-    @Override
-    public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException {
-        if (isShutdown) {
-            throw new IllegalStateException("ExecutorService is shutdown");
-        }
-        List<Future<T>> futures = new ArrayList<>();
-        for (Callable<T> task : tasks) {
-            FutureTask<T> futureTask = new FutureTask<>(task);
-            submit(futureTask);
-            futures.add(futureTask);
-        }
-        for (Future<?> future : futures) {
-            try {
-                future.get();
-            } catch (ExecutionException e) {
-                // Exception occurred, ignore it and move on
-            }
-        }
-        return futures;
-    }
-
-    @Override
-    public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit)
-            throws InterruptedException {
-        if (isShutdown) {
-            throw new IllegalStateException("ExecutorService is shutdown");
-        }
-        List<Future<T>> futures = new ArrayList<>();
-        for (Callable<T> task : tasks) {
-            FutureTask<T> futureTask = new FutureTask<>(task);
-            submit(futureTask);
-            futures.add(futureTask);
-        }
-        long timeoutInMs = unit.toMillis(timeout);
-        long startTime = System.currentTimeMillis();
-        for (Future<?> future : futures) {
-            try {
-                future.get(timeoutInMs - (System.currentTimeMillis() - startTime), TimeUnit.MILLISECONDS);
-            } catch (ExecutionException e) {
-                // Exception occurred, ignore it and move on
-            } catch (TimeoutException e) {
-                // Timeout occurred, ignore it and move on
-            }
-        }
-    }
-
-    @Override
-    public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
-        if (isShutdown) {
-            throw new IllegalStateException("ExecutorService is shutdown");
-        }
-        List<Future<T>> futures = new ArrayList<>();
-        for (Callable<T> task : tasks) {
-            FutureTask<T> futureTask = new FutureTask<>(task);
-            submit(futureTask);
-            futures.add(futureTask);
-        }
-        for (Future<?> future : futures) {
-            try {
-                return future.get();
-            } catch (ExecutionException e) {
-                // Exception occurred, ignore it and move on
-            }
-        }
-        throw new ExecutionException("No task completed successfully", null);
-    }
-
-    @Override
-    public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit)
-            throws InterruptedException, ExecutionException, TimeoutException {
-        if (isShutdown) {
-            throw new IllegalStateException("ExecutorService is shutdown");
-        }
-        List<Future<T>> futures = new ArrayList<>();
-        for (Callable<T> task : tasks) {
-            FutureTask<T> futureTask = new FutureTask<>(task);
-            submit(futureTask);
-            futures.add(futureTask);
-        }
-        long timeoutInMs = unit.toMillis(timeout);
-        long startTime = System.currentTimeMillis();
-        for (Future<?> future : futures) {
-            try {
-                return future.get(timeoutInMs - (System.currentTimeMillis() - startTime), TimeUnit.MILLISECONDS);
-            } catch (ExecutionException e) {
-                // Exception occurred, ignore it and move on
-            } catch (TimeoutException e) {
-                // Timeout occurred, ignore it and move on
-            }
-        }
-        throw new ExecutionException("No task completed successfully", null);
     }
 }
