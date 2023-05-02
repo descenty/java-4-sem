e0c92642aab91e1dc7fb27c52440cc6862596b09
Deverty <bychenkov.a.k@edu.mirea.ru>
2023-05-02 23:58:37 +0300

    add practice12

---
+++ practice12/src/main/java/ru/mirea/hasher/FileHasher.java
@@ -0,0 +1,69 @@
+package ru.mirea.hasher;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.security.MessageDigest;
+
+@Component
+public class FileHasher {
+    @Value("${inputFile}")
+    private String inputFile;
+
+    @Value("${outputFile}")
+    private String outputFile;
+
+    private File inputFileObj;
+    private File outputFileObj;
+
+    @PostConstruct
+    public void initialize() throws Exception {
+        inputFileObj = new File(inputFile);
+        outputFileObj = new File(outputFile);
+
+        if (!inputFileObj.exists()) {
+            outputFileObj.createNewFile();
+            try (FileOutputStream outputStream = new FileOutputStream(outputFileObj)) {
+                outputStream.write("null".getBytes());
+            }
+            return;
+        }
+
+        try (InputStream inputStream = new FileInputStream(inputFileObj)) {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] buffer = new byte[8192];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                digest.update(buffer, 0, bytesRead);
+            }
+
+            byte[] hashBytes = digest.digest();
+            String hashString = bytesToHex(hashBytes);
+
+            try (FileOutputStream outputStream = new FileOutputStream(outputFileObj)) {
+                outputStream.write(hashString.getBytes());
+            }
+        }
+
+        inputFileObj.delete();
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        if (inputFileObj.exists()) {
+            inputFileObj.delete();
+        }
+    }
+
+    private String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+}
---
+++ practice12/src/main/java/ru/mirea/practice12/Practice12Application.java
@@ -0,0 +1,12 @@
+package ru.mirea.practice12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice12Application {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Practice12Application.class, args);
+	}
+}
